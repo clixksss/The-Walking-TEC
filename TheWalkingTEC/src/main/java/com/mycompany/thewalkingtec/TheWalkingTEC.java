f2d69b5f86d81f9de85256b55ce4bd395b2ccb0b
PJAO120406 <milanesabreteadora1@gmail.com>
2025-10-06 10:44:32 -0600

    Update TheWalkingTEC.java
    hola

--- TheWalkingTEC/src/main/java/com/mycompany/thewalkingtec/TheWalkingTEC.java
+++ TheWalkingTEC/src/main/java/com/mycompany/thewalkingtec/TheWalkingTEC.java
@@ -11,6 +11,6 @@ package com.mycompany.thewalkingtec;
 public class TheWalkingTEC {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Todo listo!");
     }
 }
